c2b5874f22760c87bd03a80a58dadf6246f5f110
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2024-01-07 13:07:30 +0530

    Add files via upload

---
+++ Find_Pivot_Index.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>left(n),right(n);
+        left[0]=nums[0];
+        right[n-1]=nums[n-1];
+        for(int i=1;i<n;i++)
+            left[i]+=nums[i]+left[i-1];
+        for(int i=n-2;i>=0;i--)
+            right[i]+=nums[i]+right[i+1];
+        for(int i=0;i<n;i++)
+            if(left[i]==right[i])
+                return i;
+        return -1;
+    }
+};
